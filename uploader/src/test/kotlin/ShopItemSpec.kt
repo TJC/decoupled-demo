59a8f5f4cab2ed649643b017b33ba2230fedafeb
Toby Corkindale <toby@dryft.net>
2019-02-08 17:50:23 +1100

    use test db in tests

--- uploader/src/test/kotlin/ShopItemSpec.kt
+++ uploader/src/test/kotlin/ShopItemSpec.kt
@@ -2,28 +2,32 @@ package net.dryft.decoupled.uploader
 
 import io.kotlintest.*
 import io.kotlintest.matchers.string.shouldStartWith
-import io.kotlintest.specs.DescribeSpec
+import io.kotlintest.specs.StringSpec
 import java.io.File
 
-class ShopItemSpec : DescribeSpec({
+class ShopItemSpec : StringSpec()
+{
+    override fun beforeSpec(spec: Spec) {
+        super.beforeSpec(spec)
+        DBManager.setup("test.db")
+    }
+
     fun getImage() : File {
         val imgSrc = this.javaClass.getResource("/lorem-ipsum.jpg").toURI()
         return File(imgSrc)
     }
 
-    DBManager.setup(":memory")
-
-    describe("ShopItem") {
-        it("creates an item") {
+    init {
+        "creates an item" {
             val item = ShopItem.create("test item #1", "Sample description", getImage())
             item.title shouldBe "test item #1"
         }
 
-        it("can find a created item") {
+         "can find a created item" {
             val item = ShopItem.create("test item #2", "Sample description", getImage())
             val item2 = ShopItem.find(item.id)
             item2 shouldNotBe null
             if (item2 != null) item2.title shouldBe "test item #2"
         }
     }
-})
+}
